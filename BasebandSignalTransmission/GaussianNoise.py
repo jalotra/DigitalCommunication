27359beea558241fed8f5a9240e79b77ac100111
Shivam <jalotrashivam9@gmail.com>
2019-08-02 02:10:14 +0530

    Changed the class name

--- BasebandSignalTransmission/GaussianNoise.py
+++ BasebandSignalTransmission/GaussianNoise.py
@@ -15,8 +15,8 @@ class GaussianNoise(object):
         self.name = name 
         self.pi = numpy.pi
 
-    def gaussian_white_noise(self, number_of_samples,scaling_factor, mean = 0, standard_deviation = 1):
-        samples = numpy.random.normal(mean, standard_deviation, size = number_of_samples + 1 )
+    def gaussian_white_noise(self, number_of_samples, scaling_factor, mean = 0, standard_deviation = 1):
+        samples = numpy.random.normal(mean, standard_deviation, size = 4*number_of_samples + 1 )
         return numpy.dot(scaling_factor, samples)
         
     # SAMPLES ARE OF THE TYPE numpy.ndarray
@@ -27,28 +27,26 @@ class GaussianNoise(object):
     def sampling_frequency(self, number_of_samples):
         samplingFrequency = (2*self.pi/number_of_samples)
         timeStamps = []
-        x = 0
-        while(x <= 2*self.pi):
+        x = -4*self.pi
+        while(x <= 4*self.pi):
             timeStamps.append(x)
             x += samplingFrequency
 
         return timeStamps
-
-
 
     def plot_gaussian_white_noise(self, number_of_samples ,scaling_factor):
         plt.plot(self.sampling_frequency(number_of_samples), self.gaussian_white_noise(number_of_samples, scaling_factor))
         plt.title('GAUSSIAN WHITE NOISE WITH STANDARD DEVIATION : 1 AND TIME PERIOD 2pi')
         plt.xlabel('TimePeriod')
         plt.ylabel('Amplitude of Noise')
-        plt.xticks([x for x in numpy.arange(0, 2*self.pi +1 , self.pi/4)])
+        plt.xticks([x for x in numpy.arange(-4*self.pi, 4*self.pi +1 , self.pi/2)])
         plt.yticks(numpy.arange(-4*scaling_factor , 4*scaling_factor , 1))
         plt.show()
 
 
 if __name__ == "__main__":
     def plot():
-        WhiteNoiseObject = Noise('GAUSSIAN WHITE NOISE')
+        WhiteNoiseObject = GaussianNoise('GAUSSIAN WHITE NOISE')
         print("#################################################################################")
         print(WhiteNoiseObject.name)
         print()
@@ -56,7 +54,7 @@ if __name__ == "__main__":
         print("#################################################################################")
         WhiteNoiseObject.plot_gaussian_white_noise(number_of_samples,scaling_factor = 1)
     def print_samples():
-        WhiteNoiseObject = Noise('GAUSSIAN WHITE NOISE')
+        WhiteNoiseObject = GaussianNoise('GAUSSIAN WHITE NOISE')
         print("#################################################################################")
         print(WhiteNoiseObject.name)
         print()
