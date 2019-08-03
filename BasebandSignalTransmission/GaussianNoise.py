76d64e72a04f7b9908e301dcdda2a1d48341e79f
Shivam <jalotrashivam9@gmail.com>
2019-08-03 08:47:42 +0530

    Added method to use seaborn as plotter

--- BasebandSignalTransmission/GaussianNoise.py
+++ BasebandSignalTransmission/GaussianNoise.py
@@ -8,15 +8,18 @@
 # FROM SAMPLES TAKEN BY THE white_noise function   
 import numpy
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 class GaussianNoise(object):
-    def __init__(self, name):
+    def __init__(self, standard_deviation, name):
         self.name = name 
         self.pi = numpy.pi
+        self.standard_deviation = standard_deviation
 
-    def gaussian_white_noise(self, number_of_samples, scaling_factor, mean = 0, standard_deviation = 1):
-        samples = numpy.random.normal(mean, standard_deviation, size = 4*number_of_samples + 1 )
+    def gaussian_white_noise(self, number_of_samples, scaling_factor, mean = 0):
+        samples = numpy.random.normal(mean, self.standard_deviation, size = 4*number_of_samples + 1 )
         return numpy.dot(scaling_factor, samples)
         
     # SAMPLES ARE OF THE TYPE numpy.ndarray
@@ -43,26 +46,43 @@ class GaussianNoise(object):
         plt.yticks(numpy.arange(-4*scaling_factor , 4*scaling_factor , 1))
         plt.show()
 
+    def seaborn_plot_gaussian(self, number_of_samples, scaling_factor):
+        df = pd.DataFrame(dict(TimeStamps = self.sampling_frequency(number_of_samples),
+            NoiseAmplitude = self.gaussian_white_noise(number_of_samples, scaling_factor)))
+        g = sns.relplot(x= "TimeStamps", y= "NoiseAmplitude", kind="line", data= df)
+        plt.show(g)
+
 
 if __name__ == "__main__":
     def plot():
-        WhiteNoiseObject = GaussianNoise('GAUSSIAN WHITE NOISE')
+        WhiteNoiseObject = GaussianNoise(1.2, 'GAUSSIAN WHITE NOISE')
         print("#################################################################################")
         print(WhiteNoiseObject.name)
         print()
         number_of_samples = int(input('Total number of samples \n'))
         print("#################################################################################")
         WhiteNoiseObject.plot_gaussian_white_noise(number_of_samples,scaling_factor = 1)
+    
     def print_samples():
-        WhiteNoiseObject = GaussianNoise('GAUSSIAN WHITE NOISE')
+        WhiteNoiseObject = GaussianNoise (1.2, 'GAUSSIAN WHITE NOISE')
         print("#################################################################################")
         print(WhiteNoiseObject.name)
         print()
         number_of_samples = int(input('Total number of samples \n'))
+        scaling_factor = int(input('Enter the scaling factor to increase or decrease the amplitude of noise '))
         print("#################################################################################")
         WhiteNoiseObject.print_gaussian_white_noise_samples(number_of_samples, scaling_factor = 1)
     
-    print_samples()
-    #print_samples()
+    def seaborn_plot():
+        
+        WhiteNoiseObject = GaussianNoise(0.9, 'GAUSSIAN WHITE NOISE')
+        print("#################################################################################")
+        print(WhiteNoiseObject.name)
+        print()
+        number_of_samples = int(input('Total number of samples \n'))
+        scaling_factor = int(input('Enter the scaling factor to increase or decrease the amplitude of noise '))
+        print("#################################################################################")
+        WhiteNoiseObject.seaborn_plot_gaussian(number_of_samples, scaling_factor)
 
+    seaborn_plot()
 
