3322c98663080a958f06b6e8b16abc68353bfd88
Shivam <jalotrashivam9@gmail.com>
2019-08-05 21:24:00 +0530

    Rightly done with time shifting and T time_period adding

--- BasebandSignalTransmission/MatchedFilter.py
+++ BasebandSignalTransmission/MatchedFilter.py
@@ -1,66 +1,56 @@
-import numpy 
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas
+import seaborn as sns
+import pandas as pd
 
 
 import BasebandWaveforms as BW 
 
+''' This class represents the matched filter response only for a pulse wave'''
 class MatchedFilterResponse(object):
-    def __init__(self):
-        self.pi = numpy.pi
-        self.time_period = 2*self.pi
-
-    def signal_to_process(self, number_of_samples):
-        timeStamps = numpy.arange(0, self.time_period, (self.time_period/(2*number_of_samples)))
-        # print(timeStamps)
-        # print(len(timeStamps))
-        unit_step_vector = []
-
-        for value in timeStamps:
-            if value > 0 :
-                unit_step_vector.append(1)
-            elif value == 0 : 
-                unit_step_vector.append(0.5)
-            else:
-                unit_step_vector.append(0)
-        unit_step_vector[-1] = 0 
-        unit_step_vector[0] = 0
-        # print(unit_step_vector)
-        # print(len(unit_step_vector))
-        return (timeStamps, unit_step_vector)       
-
-    def plotting_signal_to_process(self):
-        x_values , y_values = self.signal_to_process(4001)
-        plt.plot(x_values , y_values)
-        plt.title('g(t) ')
-        plt.xlabel('< -- TimePeriod -- >')
-        plt.ylabel('< -- Amplitude -- >')
-        plt.xticks([x for x in numpy.arange(0, self.time_period +1 , self.pi/2)])
-        plt.show()
-
-
-    def impulse_response_filter(self, K):
-    # By theory h(t) = Kg(T-t) where T is the sampling Time Period
-    # If the function looks something like this u(t) - u(T-t) which is only a single pulse
-    # h(t) = Kg(t) Can be seen by plotting the graph
+    def __init__(self, time_period):
+        self.time_period = time_period
     
-        if( -8*self.pi <= self.time_period <=  8*self.pi ):
-            y_vector = numpy.dot(K, self.signal_to_process(2001)[1])
+    # Impulse response of a matched can be represnented as K*g(T-t)
+    def impulse_response(self, K, duty_cycle, decimal_number_to_send, number_of_samples):
+        timeStamps_pulse_vector, value_pulse_vector = BW.BasebandWaveforms.pulse_function(BW.BasebandWaveforms(self.time_period),
+         duty_cycle, decimal_number_to_send, number_of_samples)
 
-        return (self.signal_to_process(4001)[0] , y_vector)
+        # To return a vector that represents a matched filter response 
+        # I have to mutiply each timeStamp with -1
+        # Then add self.time_period to each timeStamp 
+        # Job will be done
 
-    def plotting_impulse_response_filter(self, K):
-        x_values , y_values = self.impulse_response_filter(K)
-        plt.plot(x_values , y_values)
-        plt.title('h(t) ')
-        plt.xlabel('< -- TimePeriod -- >')
-        plt.ylabel('< -- Amplitude -- >')
-        plt.xticks([x for x in numpy.arange(0, self.time_period +1 , self.pi/2)])
+        # print(type(timeStamps_pulse_vector))
+        timeStampsVector = []
+        for elements in timeStamps_pulse_vector:
+            elements *= -1  
+            elements +=  self.time_period
+            timeStampsVector =  np.append(timeStampsVector, elements)
+            
+           
+        # print(len(timeStamps_pulse_vector))
+        # print(len(timeStampsVector))
+        return timeStampsVector, np.dot(value_pulse_vector, K)
+    
+    def plot_matched_filter_response(self, K, duty_cycle, decimal_number_to_send, number_of_samples):
+        df = pd.DataFrame(dict(TimeStamps= self.impulse_response(K, duty_cycle, decimal_number_to_send, number_of_samples)[0],
+                              Amplitude = self.impulse_response(K, duty_cycle, decimal_number_to_send, number_of_samples)[1]))
+        g = sns.relplot(x= "TimeStamps", y= "Amplitude", kind="line", data= df)
+        df.head()
+        plt.grid(color='black', linestyle='-', linewidth=.5)
+        plt.title('ORIGINAL SIGNAL THAT HAS TO BE TRANSMITTED REPRESENTING {} IN BINARY'.format(decimal_number_to_send))
         plt.show()
+       
+
+
+    
 
 
 if __name__ == "__main__":
-    OBJ = MatchedFilterResponse()
+    OBJ = MatchedFilterResponse(10)
     # OBJ.plotting_signal_to_process()
-    OBJ.plotting_impulse_response_filter(3.5)
+    OBJ.plot_matched_filter_response(K = 3, duty_cycle = 50, decimal_number_to_send = 50, number_of_samples = 500)
     # OBJ.plotting_signal_to_process()
     
