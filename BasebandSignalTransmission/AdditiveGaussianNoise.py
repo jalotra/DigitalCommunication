e63b9938bc8b7f2c4642558f442cd8fb8761431b
Shivam <jalotrashivam9@gmail.com>
2019-08-05 18:00:52 +0530

    Completed code to add white noise to a series of bits that have to be transferred

--- BasebandSignalTransmission/AdditiveGaussianNoise.py
+++ BasebandSignalTransmission/AdditiveGaussianNoise.py
@@ -8,22 +8,30 @@ import GaussianNoise as GN
 
 # scaling_factor = 2
 
-def add_noise_to_functions(scaling_factor):
-    x_unit_step_vector, y_unit_step_vector = BW.BasebandWaveforms.pulse_function(BW.BasebandWaveforms(), 4001)
-    y_noise_vector = GN.GaussianNoise.gaussian_white_noise(GN.GaussianNoise('Gaussian White Noise'), number_of_samples = 1000, scaling_factor = scaling_factor)
+def add_noise_to_functions(scaling_factor, decimal_number_to_send, time_period, standard_deviation, number_of_samples, duty_cycle, starting_time, ending_time):
+    timeStamps_pulse_vector, value_pulse_vector = BW.BasebandWaveforms.pulse_function(BW.BasebandWaveforms(time_period), duty_cycle, decimal_number_to_send, number_of_samples)
+    y_noise_vector = GN.GaussianNoise.gaussian_white_noise(GN.GaussianNoise(standard_deviation, starting_time, ending_time, time_period) ,
+    number_of_samples, scaling_factor )
 
-    resultant_waveform_y_values = numpy.add(y_unit_step_vector, y_noise_vector)
+    resultant_waveform_y_values = numpy.add(value_pulse_vector, y_noise_vector)
 
-    return (x_unit_step_vector, resultant_waveform_y_values)
+    return (timeStamps_pulse_vector, resultant_waveform_y_values)
 
 
-def plotting_the_distorted_wave(scaling_factor):
-    x_timestamps, y_values  = add_noise_to_functions(scaling_factor)
+def plotting_the_distorted_wave(scaling_factor, decimal_number_to_send, time_period, standard_deviation, number_of_samples, duty_cycle, starting_time, ending_time):
+    x_timestamps, y_values  = add_noise_to_functions(scaling_factor, decimal_number_to_send, time_period, 
+    standard_deviation, number_of_samples, duty_cycle, starting_time, ending_time)
     plt.plot(x_timestamps, y_values)
-    plt.title('DISTORTED WAVE WITH NOISE POWER THAT IS {}00%'.format(300*scaling_factor))
+    plt.title('DISTORTED WAVE WITH NOISE AMPLITUDE THAT IS {}% OF THE BASEBAND SIGNAL'.format(standard_deviation*100))
     plt.xlabel('TimePeriod')
     plt.ylabel('Amplitude of the Distorted Wave')
+    plt.grid(color='black', linestyle='-', linewidth=.5)
     plt.show()
 
 if __name__ == "__main__":
-    plotting_the_distorted_wave(1.2)
+
+    time_period = 10
+    decimal_number = 16
+    count = BW.BasebandWaveforms.decimal_to_binary(BW.BasebandWaveforms(time_period = time_period),decimal_number = decimal_number)[1]
+    plotting_the_distorted_wave(scaling_factor = 1, decimal_number_to_send = decimal_number, time_period = time_period , standard_deviation = 0.01, 
+    number_of_samples = 200, duty_cycle = 50, starting_time = 0 , ending_time = count*time_period )
